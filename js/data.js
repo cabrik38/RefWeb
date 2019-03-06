d0d8bdc6ddd40e6d35e21cad1b7ab21bfec60f32
dorian <cabrik.ben@gmail.com>
2019-03-06 09:35:15 +0100

    first commit

---
+++ js/data.js
@@ -0,0 +1,26 @@
+const heroes = {
+  'marvel' : {
+      '1': {
+        'title' : 'storm',
+        'prenom': 'ororo',
+        'nom' : 'munroe',
+        'pseudo': 'storm',
+        'pouvoirs': 'maitrise de la météo',
+        'taille' : '1m75',
+        'presentation': 'Tornade est une mutante qui possède la capacité de contrôler psioniquement la météo. Elle peut déclencher la pluie, des vents d’intensité différente, augmenter ou abaisser le taux d’humidité, de la température et même celui de la lumière.
+            Elle peut créer des éclairs en les dirigeant directement de ses mains vers ses ennemis et d’autres phénomènes électriques, disperser des tempêtes.
+            Ses pouvoirs peuvent se manifester sur de très grandes surfaces comme sur de toutes petites : par exemple elle peut faire apparaître un nuage autour d’une simple plante pour l’arroser.
+            Ses pouvoirs ne peuvent créer ou modifier que des conditions climatiques qui existent sur la planète où elle se trouve.
+            Tornade est limitée par la force de sa volonté et de son corps.
+            Cela peut lui prendre des heures pour calmer des blizzards de très forte intensité et peut la laisser dans un état proche de la mort. Elle peut lever des vents capable de l’élever dans les airs. Quand elle vole, elle peut emmener des passagers avec elle en générant des vents assez forts. Ses pouvoirs lui permettent d’annuler sur elle les effets de la friction du vent quand elle vole, du froid et de la chaleur extrême.Elle est aussi capable d’altérer sa vision pour percevoir son environnement en terme d’énergie électrique. Enfin ses pouvoirs la protègent des changements de températures, ainsi en Antarctique elle ne craint pas le froid, et dans le désert, elle ne craint pas la chaleur. Enfin, elle est connectée à la terre et elle est capable de ressentir de gros changements climatiques qui peuvent même la blesser. Quand elle utilise ses pouvoirs, ses pupilles disparaissent et ses yeux sont blancs.',
+        'passe':'Chef des x-men apres la mort de Wolverine elle désigne Kitty Pryde pour sa succession',
+        'present': 'Elle combat au côté des X-mens dans leurs différentes aventures, elle a un role moins important aujourd\'hui.',
+        'futur' : 'StormBlood : Mordu par dracula elle est obligé de se nourrir de sang malgré son envie d\'être une héroine. Elle rejoint le présent de la terre 616 pour aider les jeunes x-men',
+      }
+    },
+  'dc' : {
+    '1' : {
+      
+    }
+  }
+};
---
+++ js/insertion.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  alert("ezokpokfpofkeop");
+    loadAccueil();
+
+    //loadAsyncMenu();
+    // $(".menu ul a").click(function () {
+    //     //loadingEditeur($(this));
+    // });
+});
+
+
+function loadAccueil(){
+  $({}).load('Typage.html', function(data) {
+      $("#container").html(data);
+      // alert(data);
+      // console.log(data);
+      // $.each(heroes, function (index, edition) {
+      //     $.each(recetteByCat, function (indexRecette, value) {
+      //         createHomeAsyncRecettes(index, indexRecette, value);
+      //     });
+      // })
+  });
+}
